4ff09fde9bf5a44958fff46a702f2013f1c8ba2c
Piotr Lesiak <piolesiak12@gmail.com>
2025-01-09 12:52:38 +0100

    fix: add font subsets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { ReactNode } from 'react';
 
 import './globals.css';
 
-const globalFont = Lexend();
+const globalFont = Lexend({ subsets: ['latin'] });
 
 // prettier-ignore
 export const metadata: Metadata = {
